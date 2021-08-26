ad721eedc6e104bdcb9da585fda32165c8ae2cd4
CarlosFratini <carlosfratini@gmail.com>
2021-08-25 21:55:56 -0300

    Libft Header file

---
+++ libft.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ceduard2 <ceduard2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/23 14:38:01 by ceduard2          #+#    #+#             */
+/*   Updated: 2021/08/25 21:45:18 by ceduard2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+int		ft_atoi(const char *nptr);
+void	ft_bzero(void *b, size_t len);
+void	*calloc(size_t num, size_t size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memchr(const void *b, int c, size_t len);
+int		ft_memcmp(const void *b1, const void *b2, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *dst, int c, size_t len);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t len);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*strdup(const char *str);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+#endif
